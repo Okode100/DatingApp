5fbcaa25234125875fbf517a452d3931c05fe5d4
Moses Okode <mokode@seaburysolutions.com>
2026-09-09 20:13:06 +0300

    Create Api exe file

--- obj/Debug/net10.0/API.AssemblyInfo.cs
+++ obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3dd79392aa1b8ca373e59d2ddae5a9644294c5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f5f24db283e87742da288dcbebbb5a87bcf453")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
